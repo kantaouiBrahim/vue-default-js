c5cb757711c79212fa6b0fbf3194b6765712ebd8
kantaouiBrahim <kantaouibrahim@gmail.com>
2023-08-08 21:22:57 -0700

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>{{ count }}</h1>
+  <input type="text" v-model.number="incValue" />
+  <button @click="inc">inc</button>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const count = ref(0);
+const incValue = ref(1);
+
+function inc() {
+  count.value += incValue.value;
+}
+</script>
+
+<style scoped></style>
