91348527eb4f28bd40060c8461dd41c24a6fc74c
kantaouiBrahim <kantaouibrahim@gmail.com>
2023-08-09 05:20:52 -0700

    Added Wave-UI CSS Framework

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+import WaveUI from "wave-ui";
+import "wave-ui/dist/wave-ui.css";
+const app = createApp(App);
+
+app.use(WaveUI, {});
+
+app.mount("#app");
